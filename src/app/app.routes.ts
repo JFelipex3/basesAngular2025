6bf6052a3e096ec633e6e6afc08b1cddba73f4b5
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-02-21 17:26:53 -0300

    Sección 4 Angular - Clase 40 y 41: Tarea Curso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
 
 export const routes: Routes = [
   {path: 'counter', component: CounterPageComponent},
+  {path: 'hero', component: HeroPageComponent},
   {path: '**', redirectTo: 'counter'}
 ];
