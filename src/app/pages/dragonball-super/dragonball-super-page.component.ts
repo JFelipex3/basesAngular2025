3148aca6d352bf18b7d61ecfdc77a39d387910e1
Jhonnatan Machuca <jfelipe.machuca@gmail.com>
2025-02-26 19:31:19 -0300

    Sección 5: Clase 60 - Uso de servicios

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,25 +1,14 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CharacterLisComponent } from "../../components/dragonball/character-lis/character-lis.component";
-import { Character } from "../../interfaces/character.interface";
 import { CharacterAddComponent } from "../../components/dragonball/character-lis/character-add/character-add.component";
+import { DragonballService } from "../../services/dragonball.service";
 
 @Component({
-  selector: 'app-dragonball-super-page',
+  selector: 'dragonball-super-page',
   imports: [CharacterLisComponent, CharacterAddComponent],
   templateUrl: './dragonball-super-page.component.html',
   styleUrl: './dragonball-super-page.component.css'
 })
 export class DragonballSuperPageComponent {
-
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8000}
-  ]);
-
-  addCharacter(newCharacter: Character) {
-    this.characters.update((list) => [... list, newCharacter]);
-  }
+  public dragonballService = inject(DragonballService);
 }
